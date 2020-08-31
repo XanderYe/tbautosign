1e4b59a59c950bdcb34901c3e7e7d241030414cf
XanderYe <XanderYe@outlook.com>
2020-08-31 15:27:33 +0800

    修复番剧解析bug

--- api/src/main/java/cn/xanderye/tbautosign/controller/ToolController.java
+++ api/src/main/java/cn/xanderye/tbautosign/controller/ToolController.java
@@ -1,9 +1,12 @@
 package cn.xanderye.tbautosign.controller;
 
 import cn.xanderye.tbautosign.base.ResultBean;
+import cn.xanderye.tbautosign.enums.ErrorCode;
 import cn.xanderye.tbautosign.exception.BusinessException;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author XanderYe
  */
+@Slf4j
 @RestController
 @RequestMapping("tool")
 public class ToolController {
@@ -32,6 +36,24 @@ public class ToolController {
     @PostMapping("danmu")
     public ResultBean danmu(@RequestBody JSONObject params) {
         String url = params.getString("url");
+        if (StringUtils.isEmpty(url)) {
+            throw new BusinessException(ErrorCode.PARAMETER_EMPTY);
+        }
+        String cid;
+        if (url.contains("bangumi")) {
+            cid = bangumiUrl(url);
+        } else {
+            cid = normalUrl(url);
+        }
+        if (cid == null) {
+            throw new BusinessException(url + "解析错误");
+        }
+        String resLink = "https://api.bilibili.com/x/v1/dm/list.so?oid=" + cid;
+        log.info("url:{}, commentUrl:{}", url, resLink);
+        return new ResultBean(resLink);
+    }
+
+    public String normalUrl(String url) {
         String cid = null;
         try {
             Document doc = Jsoup.connect(url)
@@ -39,22 +61,52 @@ public class ToolController {
                     .get();
             Elements elements = doc.select("head");
             Elements scripts = elements.get(0).select("script");
-             JSONObject initialStateJson = null;
+            JSONObject initialStateJson = null;
             for (Element element : scripts) {
                 if (element.toString().contains("__INITIAL_STATE__")) {
                     String initialState = StringUtils.substringBetween(element.toString(), "__INITIAL_STATE__=", ";(function");
                     initialStateJson = JSON.parseObject(initialState);
+                    break;
                 }
             }
             String embedPlayer = initialStateJson.getJSONObject("videoData").getString("embedPlayer");
             cid = StringUtils.substringBetween(embedPlayer, "cid=", "&aid");
-        } catch (Exception e) {
-            throw new BusinessException("解析错误");
+        } catch (Exception ignored) {
+
         }
-        if (cid == null) {
-            throw new BusinessException("解析错误");
+        return cid;
+    }
+
+    public String bangumiUrl(String url) {
+        String cid = null;
+        try {
+            Long id = Long.valueOf(url.substring(url.lastIndexOf("ep") + 2));
+            Document doc = Jsoup.connect(url)
+                    .userAgent("5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36")
+                    .get();
+            Elements elements = doc.select("body");
+            Elements scripts = elements.get(0).select("script");
+            JSONObject initialStateJson = null;
+            for (Element element : scripts) {
+                if (element.toString().contains("__INITIAL_STATE__")) {
+                    String initialState = StringUtils.substringBetween(element.toString(), "__INITIAL_STATE__=", ";(function");
+                    initialStateJson = JSON.parseObject(initialState);
+                    break;
+                }
+            }
+            JSONArray epList = initialStateJson.getJSONArray("epList");
+            if (epList != null && epList.size() > 0) {
+                for (int i = 0; i < epList.size(); i++) {
+                    JSONObject jsonObject = epList.getJSONObject(i);
+                    if (id.equals(jsonObject.getLong("id"))) {
+                        cid = jsonObject.getString("cid");
+                        break;
+                    }
+                }
+            }
+        } catch (Exception ignored) {
+
         }
-        String resLink = "https://api.bilibili.com/x/v1/dm/list.so?oid=" + cid;
-        return new ResultBean(resLink);
+        return cid;
     }
 }
